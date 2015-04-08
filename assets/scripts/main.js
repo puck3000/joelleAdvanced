a1c284def715b5963fc648f1cc1decdd1d989ffb
puck3000 <daniel.lerch@gmail.com>
2015-04-08 14:05:44 +0200

    Updates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,1 @@
-$(function(){$("#slides").superslides({pagination:!1}),$("#navToggle").click(function(){$(".info").fadeToggle(),console.log("toggle!x")}),$("#winthiLink").click(function(){$("#winthiLink").fadeOut()})});
+$(function(){$("#slides").superslides({pagination:!1}),$("#navToggle").click(function(){$(".info").fadeToggle(),$("#portfolio").fadeToggle(),console.log("toggle!x")}),$("#winthiLink").click(function(){$("#winthiLink").fadeOut()})});
